5834904eec86898e090769a7c1e9b0bfd5683fdb
bneil006 <brmneil2@gmail.com>
2025-02-12 22:57:21 -0600

    adding progress

--- main.py
+++ main.py
@@ -1,42 +1,60 @@
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
-def __init_game():
-    sucesses, failures = pygame.init()
+
+def main():
     pygame.init()
-    print(f"""
-PYGAME START RESULTS
-___________________________________________
-         
-SUCCESSES: {sucesses}, FAILURES: {failures}
-___________________________________________
-            """)
-
-def __game_start():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}") 
-
-def game_running():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    running = True
-    while running:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        
-        screen.fill((32, 31, 30))
-        pygame.display.flip()
-    
 
-def main():
-    __init_game()
-    __game_start()
-    game_running()
-    
-    
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.kill()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
